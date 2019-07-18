cc6a9ca24e55684456d9ef1ff669884db4c3183d
Kevin Tou <toukevin@gmail.com>
2019-07-18 15:02:56 -0700

    Updated to make card data default to current date

--- src/App.js
+++ src/App.js
@@ -10,16 +10,25 @@ import DateSelector from "./components/DateSelector";
 
 function App() {
   const [data, setData] = useState();
-  const [date, setDate] = useState("2019-07-17");
+  const [date, setDate] = useState();
 
   useEffect(() => {
     const getData = async () => {
-      return await axios.get("https://api.nasa.gov/planetary/apod", {
-        params: {
-          date: date,
-          api_key: "DEMO_KEY"
-        }
-      });
+      return await axios.get(
+        "https://api.nasa.gov/planetary/apod",
+        date
+          ? {
+              params: {
+                date: date,
+                api_key: "DEMO_KEY"
+              }
+            }
+          : {
+              params: {
+                api_key: "DEMO_KEY"
+              }
+            }
+      );
     };
 
     getData()
